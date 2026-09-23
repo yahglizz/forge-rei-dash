d29f17dbe8f3a23333d335e432cccf599d7a8298
yahglizz <yahjair@atouchofblessing.com>
2026-09-23 00:17:44 -0400

    auto-sync: Mac 2026-09-23 00:17:44

---
+++ ios/ForgeMobile/App/ForgeMobileApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+@main
+struct ForgeMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .ignoresSafeArea()
+                .preferredColorScheme(.light)
+        }
+    }
+}
